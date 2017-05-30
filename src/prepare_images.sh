409d2ee0944d552860d4b379556c070122c84b40
J. Santos <jamillo@gmail.com>
2017-05-30 15:34:07 -0300

    Criação dos diagramas de sequência do sistema

--- src/prepare_images.sh
+++ src/prepare_images.sh
@@ -2,5 +2,5 @@
 for f in imagens/svg/*.svg; do
 	pdfimage="${f%.*}.pdf"
 	echo "Exporting: $f to $pdfimage"
-	inkscape -D -z "--file=$f" "--export-pdf=$pdfimage"
+	inkscape -D -z "--file=$f" "--export-pdf=$pdfimage" --export-latex
 done
