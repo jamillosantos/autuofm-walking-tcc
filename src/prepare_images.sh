23a81b50030770fb7c5c3fa064b453673ce31bca
J. Santos <jamillo@gmail.com>
2017-06-16 09:24:35 -0300

    Atualizações no capítulo 3

--- src/prepare_images.sh
+++ src/prepare_images.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 for f in imagens/svg/*.svg; do
 	pdfimage="${f%.*}.pdf"
+	pngimage="${f%.*}.png"
 	echo "Exporting: $f to $pdfimage"
 	inkscape -D -z "--file=$f" "--export-pdf=$pdfimage" --export-latex
+	inkscape -D -z "--file=$f" "--export-png=$pngimage" -w=2000
 done
+mv imagens/svg/*.png imagens/png
